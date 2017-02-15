59cba04d9b26a3a8c4a3cd2b7dac52c3600debcc
Ryan Miller <rozifus@gmail.com>
2017-02-15 23:01:26 +1100

    break up ISequencer interface, add string IIncrementer

--- projects/Rozifus.SequenceFun.ConsoleApp/Program.cs
+++ projects/Rozifus.SequenceFun.ConsoleApp/Program.cs
@@ -1,21 +1,37 @@
 using System;
+using System.Collections.Generic;
 using Rozifus.SequenceFun.IntSequences;
+using Rozifus.SequenceFun.StringSequences;
+using Rozifus.SequenceFun.Interfaces;
 
 namespace Rozifus.SequenceFun.ConsoleApp
 {
     static class App {
         static void Main(string[] args)
         {
-            var intSeq = new IntegerSequencer();
-            Console.WriteLine(intSeq.Value);
-            for (var x=0; x<5; x++) {
-                intSeq = (IntegerSequencer)intSeq.Next();
-                Console.WriteLine(intSeq.Value);
+            var incers = new List<IIncrementer> { 
+                new IntegerSequencer(0), 
+                new IntegerSequencer(16),
+                new LowerAlphaIncrementer("a"),
+                new LowerAlphaIncrementer("ay")  
+            };
+
+            PrintAll(incers);
+
+            for (var x=0; x<8; x++) {
+                IncrementAll(incers);
+                PrintAll(incers);
             }
-            for (var x=0; x<10; x++) {
-                intSeq = (IntegerSequencer)intSeq.Prev();
-                Console.WriteLine(intSeq.Value);
+        }
+
+        private static void IncrementAll(IEnumerable<IIncrementer> incrementers) {
+            foreach (var incer in incrementers) {
+                incer.Next();
             }
+        }
+
+        private static void PrintAll(IEnumerable<IElement> elements) {
+            Console.WriteLine(string.Join(" ", elements));
         }
     }
 }
